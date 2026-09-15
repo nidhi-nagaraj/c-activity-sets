cdcdf5073966c3fa0038edada2c10ef2958925fa
Nidhi Nagaraj <nidhinagaraj00@gmail.com>
2026-09-15 22:38:19 +0530

    C Program to Print Name

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,1 +1,9 @@
 // 1. Write a C program to print your name.
+#include <stdio.h>
+int main() {
+    char name[100];
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Hello %s\n", name);
+    return 0;
+}
