6a85a274a6e08426990cb70808d93382b091b06e
Nidhi Nagaraj <nidhinagaraj00@gmail.com>
2026-09-16 21:45:16 +0530

    C Program to Add Numbers

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,30 @@
 //2. Write a C program to add two numbers.
+#include <stdio.h>
 
+void input(int *aa, int *ab);
+int add(int a, int b);
+void output(int a, int b, int sum);
+int main()
+{
+    int a, b, sum;
+    input(&a, &b);       
+    sum = add(a, b);     
+    output(a, b, sum);   
+
+    return 0;
+}
+
+void input(int *aa, int *ab)
+{
+    printf("Enter two numbers: ");
+    scanf("%d %d", aa, ab);   
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+void output(int a, int b, int sum)
+{
+    printf("%d + %d = %d\n", a, b, sum);
+}
